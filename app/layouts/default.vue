bd5725a350e7548a8ca527edd9e6e3be37e7df1b
Konyuka <michaelsaiba84@gmail.com>
2025-12-17 16:05:00 +0300

    Add default layout with header, footer, and Vercel Analytics integration.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -215,11 +215,14 @@
     </footer>
     <!-- footer-end -->
     <SpeedInsights />
+    <Analytics />
   </div>
 </template>
 
 <script setup>
 import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/nuxt'
+
 import { onMounted } from 'vue'
 
 onMounted(() => {
